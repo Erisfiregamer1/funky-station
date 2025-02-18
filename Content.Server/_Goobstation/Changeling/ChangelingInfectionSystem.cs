c812bae38849851aeeef88e0d8072b099073d5dc
Eris <eris@erisws.com>
2025-02-17 19:24:42 -0500

    Oop

--- Content.Server/_Goobstation/Changeling/ChangelingInfectionSystem.cs
+++ Content.Server/_Goobstation/Changeling/ChangelingInfectionSystem.cs
@@ -62,6 +62,8 @@ public sealed partial class ChangelingInfectionSystem : EntitySystem
         if (!EntityManager.TryGetComponent(ev.Implanted.Value, out ChangelingInfectionComponent? infectComp))
             return;
 
+        infectComp.NeedsInitialization = false; // AVOID having comp reinit, that will fuck things
+
         infectComp.FirstSymptoms = _timing.CurTime + TimeSpan.FromSeconds(60f);
 
         infectComp.KnockedOut = _timing.CurTime + TimeSpan.FromSeconds(900f);
@@ -69,6 +71,8 @@ public sealed partial class ChangelingInfectionSystem : EntitySystem
         infectComp.FullyInfected = _timing.CurTime + TimeSpan.FromSeconds(2700f);
 
         infectComp.EffectsTimerDelay = 7.5f;
+
+
 
         _popupSystem.PopupEntity(Loc.GetString("changeling-convert-implant"), ev.Implanted.Value, ev.Implanted.Value, PopupType.LargeCaution);
     }
